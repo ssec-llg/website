c81ef1f43a8a2073e6bc0b0ad65d179ee2314824
mustofa-id <hm.dev7@gmail.com>
2020-06-21 19:16:03 +0700

    Using cssnano

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,12 @@
+// using cssnano
+const cssnano = require("cssnano")({
+  preset: "default"
+})
+
+
 // only needed if you want to purge
 const purgecss = require("@fullhuman/postcss-purgecss")({
-  content: ["./src/**/*.svelte", "./src/**/*.html"],
+  content: ["./src/**/*.svelte", "./src/**/*.html", "./node_modules/**/*.svelte"],
   whitelistPatterns: [/svelte-/],
   defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
 });
@@ -11,6 +17,6 @@ module.exports = {
     require('postcss-nested'),
 
     // only needed if you want to purge
-    ...(process.env.NODE_ENV === "production" ? [purgecss] : [])
+    ...(process.env.NODE_ENV === "production" ? [purgecss, cssnano] : [])
   ]
 };
