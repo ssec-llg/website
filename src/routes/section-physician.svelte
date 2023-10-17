adb789f5282683d827211217fbfd2d4ef90774f6
mustofa-id <hm.dev7@gmail.com>
2023-10-17 08:37:56 +0700

    update physician lists

--- src/routes/section-physician.svelte
+++ src/routes/section-physician.svelte
@@ -1,8 +1,6 @@
 <script>
 	import dr_ak from '$lib/assets/physician__dr-ak.png';
 	import dr_desfri from '$lib/assets/physician__dr-desfri.png';
-	import dr_faika from '$lib/assets/physician__dr-faika.png';
-	import dr_fidalia from '$lib/assets/physician__dr-fidalia.png';
 	import dr_reza from '$lib/assets/physician__dr-reza.png';
 	import dr_usman from '$lib/assets/physician__dr-usman.png';
 	import dr_wini from '$lib/assets/physician__dr-wini.png';
@@ -10,12 +8,6 @@
 
 	const physicians = [
 		// name, specialistic, image, classes
-		[
-			'DR. dr. Hj. Fidalia, Sp.M(K)', //
-			'Spesialis Mata Konsultan Glaukoma',
-			dr_fidalia,
-			'md:col-span-3'
-		],
 		[
 			'dr. H. AK. Ansyori, Sp.M(K)., M.Kes., MARS., PhD',
 			'Spesialis Mata Konsultan Vitreoretina',
@@ -47,12 +39,6 @@
 			'md:col-span-2'
 		],
 		[
-			'dr. Faika Novadianaz, Sp.M', //
-			'Spesialis Mata',
-			dr_faika,
-			'md:col-span-2'
-		],
-		[
 			'dr. M. Reza Arlas, Sp.M', //
 			'Spesialis Mata',
 			dr_reza,
@@ -66,7 +52,7 @@
 	class="py-16 bg-gradient-to-rb from-emerald-100 via-pink-100 to-violet-200"
 >
 	<p class="m-4 md:m-8 text-center text-3xl font-bold uppercase">Dokter Spesialis</p>
-	<article class="grid justify-center gap-4 mb-16 max-w-4xl md:(grid-cols-6 mt-16 mb-36 mx-a)">
+	<article class="flex flex-wrap justify-center gap-8 gap-x-16 mb-16 max-w-4xl md:(mt-16 mb-36 mx-a)">
 		{#each physicians as [name, specialistic, image, classes]}
 			<section class="p-4 text-center {classes}">
 				<picture class="flex justify-center">
